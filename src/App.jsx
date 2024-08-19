c4147d00e3513830da116d672f4f492da921fe4b
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-08-18 21:29:03 -0400

    update the project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <><h1>Your Cripto Conversor is in Development...</h1>
+    <><h1>Your Cripto Conversor is being updated...</h1>
     <h4>Check it Soon!....</h4>
     </>
   )
